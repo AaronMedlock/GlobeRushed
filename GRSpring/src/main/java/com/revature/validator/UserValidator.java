a41821bcae43d8db3c23ddac3cd2ff6f632b2b00
talhame3 <talhame3@gmail.com>
2022-01-19 09:54:39 -0800

    validation

--- GRSpring/src/main/java/com/revature/validator/UserValidator.java
+++ GRSpring/src/main/java/com/revature/validator/UserValidator.java
@@ -1,6 +1,23 @@
 package com.revature.validator;
 
-public class UserValidator 
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class UserValidator implements Validator
 {
+
+	@Override
+	public boolean supports(Class<?> clazz) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
